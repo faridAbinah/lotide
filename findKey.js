ebc5b87ee131cabbba5c322624616af3c1108777
faridAbinah <faridabinah@hotmail.com>
2021-09-16 20:41:40 -0400

    Created function that returns teh first key for which the callback returns true

---
+++ findKey.js
@@ -0,0 +1,53 @@
+/**
+ * 
+ * 
+Implement the function findKey which takes in an object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined.
+ */
+
+const findKey = function(object,callback) {
+ 
+  
+  for(let name in object) {
+
+    console.log(object[name]);
+    //We can access the value of a key using square bracket notation inside the for in loop.
+
+    if(callback(object[name])) {
+      return name;
+    }
+    
+    
+  }
+  
+}
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)); // => "noma"
+
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2),"noma")
