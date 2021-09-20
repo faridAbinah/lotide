eb6f24beda45400e138d3a395b0b274dd5d78fec
faridAbinah <faridabinah@hotmail.com>
2021-09-20 19:07:46 -0400

    Refactored functions to include mocha and chai syntax

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,16 @@
 const middle = require('../middle');
 const eqArrays = require('../eqArrays');
 const assertArraysEqual = require('../assertArraysEqual')
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1,2,7,4,5]),middle([1,2,7,4,5]));
+describe("Middle", () => {
+  //use deep equal when comparing arrays
+  it("Should return 7 ", () => {
+
+    assert.deepEqual(middle([1,2,7,4,5]),[7]);
+  })
+
+
+})
+
+//assertArraysEqual(middle([1,2,7,4,5]),middle([1,2,7,4,5]));
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,17 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 
+describe("Tail", () => {
 
+  it(" returns lighthouse,labs for yo yo, lighthouse,", () => {
+    
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]).length,2);
+  });
+
+  
+});
 
 
  // no need to capture the return value since we are not checking it
-assertEqual(tail(words).length, 2);
+//assertEqual(tail(words).length, 2);
