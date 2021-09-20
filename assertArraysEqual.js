a7cc75c4d695f3be376cc5f7e2e627968197154d
faridAbinah <faridabinah@hotmail.com>
2021-09-20 16:25:53 -0400

    Refactored function to seperate unit test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,9 @@
 
-const eqArrays = function(arrayOne,arrayTwo) {
-  let equal = false;
-
-  for (let i = 0; i < arrayOne.length; i++) {
-   
-    if (arrayOne[i] === arrayTwo[i]) {
-      equal = true;
-    } else {
-      equal = false;
-    }
-  }
-
-  return equal;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arrayOne,arrayTwo) {
   
-  console.log(arrayOne,arrayTwo);
-  console.log(eqArrays(arrayOne,arrayTwo));
+ 
 
   if (eqArrays(arrayOne,arrayTwo)) {
     return console.log(`✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`);
@@ -27,4 +13,6 @@ const assertArraysEqual = function(arrayOne,arrayTwo) {
 };
 
 
-assertArraysEqual([1, 2, 3], [1, 2, 4]);
+
+
+module.exports = assertArraysEqual;
